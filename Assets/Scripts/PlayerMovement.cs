0ad402f0fc749a13b78067cebdb5dcb774c18968
Scott Bakewell <bakewells5856@gmail.com>
2017-10-09 20:38:25 +1300

    created example project
    created an example project implementing the sync transform script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour {
+
+    //just quick movement script for demo purposes
+    //nothing interesting here
+
+    public float maxSpeed;
+    private float currentSpeed;
+
+    public void Update()
+    {
+        if (isLocalPlayer)
+        {
+            transform.Translate(currentSpeed * Time.deltaTime, 0, 0);
+            currentSpeed = Mathf.Clamp(currentSpeed - (Time.deltaTime * 5), 0, maxSpeed);
+
+            accelerate(Input.GetAxis("Vertical"));
+            turn(Input.GetAxis("Horizontal"));
+        }
+    }
+
+    public void accelerate(float axis)
+    {
+        currentSpeed = Mathf.Clamp(currentSpeed + (Mathf.Clamp(axis, 0, 1) * Time.deltaTime * 20), 0, maxSpeed);
+    }
+
+
+    public void turn(float axis)
+    {
+        transform.Rotate(0, (axis * 100 * Time.deltaTime), 0);
+    }
+}
